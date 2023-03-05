6046eaca43c72c0092e56f65d1a54ba52979480a
Akarsh <65647232+Akarsh-N-L@users.noreply.github.com>
2023-03-05 20:32:42 +0530

    Getting temprature

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const https = require("https");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("<h1>Successful</h1>");
-});
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=bengaluru&units=metric&appid=005474f74a263b53982ab3b5bef9ca48";
+    https.get(url,(response)=>{
+        console.log(response.statusCode);
+        response.on("data",(data)=>{
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            res.send("<h3>"+temp+"</h3>");
+        })
+    })
+})
+
+
 
 app.listen(3000,()=>{
     console.log("Server is up and running on port 3000..");
